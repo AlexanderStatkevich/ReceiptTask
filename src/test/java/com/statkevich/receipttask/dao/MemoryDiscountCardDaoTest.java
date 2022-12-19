800cb3eb0b354864a857d4dadea9fe705e99ecb5
bumblebear <aastatkevich@gmail.com>
2022-12-19 13:28:08 +0300

    Tests added.

---
+++ src/test/java/com/statkevich/receipttask/dao/MemoryDiscountCardDaoTest.java
@@ -0,0 +1,21 @@
+package com.statkevich.receipttask.dao;
+
+import com.statkevich.receipttask.dao.api.DiscountCardDao;
+import com.statkevich.receipttask.dao.factories.DiscountCardDaoSingleton;
+import com.statkevich.receipttask.exceptions.DataAccessException;
+import com.statkevich.receipttask.exceptions.DiscountCardNotExistException;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemoryDiscountCardDaoTest {
+
+    DiscountCardDao memoryDiscountCardDao = DiscountCardDaoSingleton.getInstance();
+
+    @Test
+    void throwsExceptionAfterFrontingInGet() {
+        Exception exception = assertThrows(DiscountCardNotExistException.class, () -> memoryDiscountCardDao.get(null));
+        assertEquals("Incorrect card number input", exception.getMessage());
+    }
+}
+    
